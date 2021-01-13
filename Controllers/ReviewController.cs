0328a4579ca00dd5fdc9677f07105685f1e57040
cristi <visanu.cristian@gmail.com>
2021-01-13 02:58:59 +0200

    added new functionality to review controller

--- Controllers/ReviewController.cs
+++ Controllers/ReviewController.cs
@@ -24,12 +24,32 @@ namespace MoviesLuv.Controllers
         }
 
         [HttpGet]
+        [Authorize(Roles = UserRole.Admin)]
+        public IActionResult Get()
+        {
+            return Ok(_db.Review.ToList());
+        }
+
+        [HttpGet]
         [Authorize(Roles = UserRole.Everyone)]
         [Route("{id}")]
         public IActionResult Get(Guid id)
         {
-            Review review = _db.Review.Find(id);
-            return Ok(review);
+            Guid userId = new Guid(User.Identity.Name);
+            Review review = _db.Review.FirstOrDefault(x => x.MovieId == id && x.UserId == userId);
+            if (review != null)
+            {
+                return Ok(review);
+            }
+            return NotFound();
+        }
+
+        [HttpGet]
+        [Authorize(Roles = UserRole.Everyone)]
+        [Route("all/{id}")]
+        public IActionResult GetAll(Guid id)
+        {
+            return Ok(_db.Review.Where(x => x.MovieId == id).ToList());
         }
 
         [HttpPost]
@@ -64,6 +84,28 @@ namespace MoviesLuv.Controllers
             }
 
             return BadRequest("User already posted a review for this movie");
+        }
+
+        [HttpDelete]
+        [Authorize(Roles = UserRole.Admin)]
+        [Route("{id}")]
+        public IActionResult DeleteReview(Guid id)
+        {
+            var review = _db.Review.Find(id);
+            if (review != null)
+            {
+                _db.Review.Remove(review);
+
+                // Here we update movie itself
+                Movie movie = _db.Movie.Find(review.MovieId);
+                movie.Rating = (movie.NrOfReviews * movie.Rating - review.Rating) / (movie.NrOfReviews - 1);
+                movie.NrOfReviews -= 1;
+                _db.Movie.Update(movie);
+
+                _db.SaveChanges();
+                return Ok();
+            }
+            return NotFound($"Movie with Id: {id} was not found.");
         }
     }
 }
